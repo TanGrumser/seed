779d66eb438ced699afbc3f87d190645d9ccc43d
derfium <marcel.mulorz@gmail.com>
2023-02-04 13:05:12 +0100

    Data and Display Textures and SHaders.

--- Assets/TextureGenerator.cs
+++ Assets/TextureGenerator.cs
@@ -1,42 +1,44 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TextureGenerator : MonoBehaviour 
 {
 
     public ComputeShader TextureShader;
-    private RenderTexture displayedTexture;
+    public ComputeShader DisplayShader;
+    private RenderTexture dataTexture;
+    private RenderTexture realDisplayTexture;
     private RenderTexture _rTexture;
     public GameObject display;
 
-    public int width = 1280;
-    public int height = 720;
 
 
     private void Start() {
-        displayedTexture = new RenderTexture(width, height, 3, RenderTextureFormat.ARGB32);
-        display.GetComponent<MeshRenderer>().material.mainTexture = displayedTexture;
-    }
+        dataTexture = new RenderTexture(Screen.width, Screen.height, 3, RenderTextureFormat.ARGB32);
+        dataTexture.enableRandomWrite = true;
+        dataTexture.Create();
 
-    private void Update() {
-        Debug.Log("OnRenderImage");
-        if (_rTexture == null) { 
-            
-            _rTexture = new RenderTexture( Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-            _rTexture.enableRandomWrite = true;
-            _rTexture.Create();
-        }
+        realDisplayTexture = new RenderTexture( Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+        realDisplayTexture.enableRandomWrite = true;
+        realDisplayTexture.Create();
+        
+        display.GetComponent<MeshRenderer>().material.mainTexture = realDisplayTexture;
 
-        TextureShader.SetTexture(0, "Result", _rTexture);
 
         int kernel = TextureShader.FindKernel("CSMain");
-        TextureShader.SetTexture(kernel, "Result", _rTexture);
+        TextureShader.SetTexture(kernel, "Result", dataTexture);
+
+        DisplayShader.SetTexture(kernel, "Source", dataTexture);
+        DisplayShader.SetTexture(kernel, "Result", realDisplayTexture);
 
         int workgroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int workgroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
 
-
         TextureShader.Dispatch(kernel, workgroupsX, workgroupsY, 1);
-        Graphics.Blit(_rTexture, displayedTexture);
-    }    
+        DisplayShader.Dispatch(kernel, workgroupsX, workgroupsY, 1);
+    }
+
+
+
 
 } 
